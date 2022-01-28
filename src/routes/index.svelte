1990dbe802a1359840ded4e4096702fb5f5803f0
SinisaVukmirovic <sinisavukmirovic@gmail.com>
2022-01-28 12:39:47 +0100

    Svelte components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = 'Ninja Gaming Guides';
+
+    const updateTitle = () => {
+        title = 'Something completelly different!';
+    }
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo id tempore nulla exercitationem, perspiciatis ratione voluptatibus cumque? Doloribus dignissimos asperiores doloremque optio nostrum, laudantium suscipit facere blanditiis voluptas eum exercitationem veritatis culpa, deleniti quasi? Suscipit exercitationem quo assumenda animi totam a quia commodi vel ipsum, quod laudantium debitis voluptates quos.</p>
+
+    <button on:click={updateTitle}>Change Title</button>
+    <input type="text" bind:value={title}>
+</div>
+
+<style>
+    .index {
+        display: block;
+        text-align: center;
+        margin: 1.5rem auto;
+    }
+</style>
