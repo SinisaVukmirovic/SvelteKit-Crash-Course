99e2d58e824d421371dc0b7f66fe9345c1a6e6f9
SinisaVukmirovic <sinisavukmirovic@gmail.com>
2022-01-28 14:56:30 +0100

    Reusable Components

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,9 @@
+<script>
+    import Title from '$lib/title.svelte';
+</script>
+
+<Title title="Ninja Gaming Reviews" />
+
 <div class="about">
     <h2>About Us</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error numquam quo excepturi voluptatibus minima ad cupiditate nemo, magnam cumque ut explicabo dolores magni optio minus, veritatis quis nesciunt porro. Commodi ea reprehenderit culpa quibusdam dolor quod esse perferendis, laudantium a error. Labore fugit exercitationem facilis dolorum voluptatem, molestiae omnis suscipit!</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,20 @@
 <script>
-    let title = 'Ninja Gaming Guides';
+    // let title = 'Ninja Gaming Guides';
 
-    const updateTitle = () => {
-        title = 'Something completelly different!';
-    }
+    // const updateTitle = () => {
+    //     title = 'Something completelly different!';
+    // }
+
+    import Title from '$lib/title.svelte';
 </script>
 
+<Title />
+
 <div class="index">
-    <h2>{title}</h2>
+    <h2>Welcome</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo id tempore nulla exercitationem, perspiciatis ratione voluptatibus cumque? Doloribus dignissimos asperiores doloremque optio nostrum, laudantium suscipit facere blanditiis voluptas eum exercitationem veritatis culpa, deleniti quasi? Suscipit exercitationem quo assumenda animi totam a quia commodi vel ipsum, quod laudantium debitis voluptates quos.</p>
     <a href="/guides">View Guides</a>
     <a href="/about">About us</a>
-
-    <button on:click={updateTitle}>Change Title</button>
-    <input type="text" bind:value={title}>
 </div>
 
 <style>
