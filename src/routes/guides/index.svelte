2fcb7122ad4e11fe9582fc7b30175c642dc80d0c
SinisaVukmirovic <sinisavukmirovic@gmail.com>
2022-02-10 15:02:03 +0100

    Displaying API data

--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -8,17 +8,38 @@
         const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
         const res = await fetch(apiUrl);
         const guides = await res.json();
+
+        if (res.ok) {
+            return {
+                props: {
+                    guides
+                }
+            }
+        }
+
+        return {
+            status: res.status,
+            error: new Error('Could not fetch the guides!')
+        }
     }
 </script>
 
 <script>
-
+    export let guides;
 </script>
 
 <div class="guides">
-    <ul>
+    <!-- <ul>
         <li><a href="/">Guide 1</a></li>
         <li><a href="/">Guide 2</a></li>
+    </ul> -->
+    <!-- replacing the above with the API data -->
+    <ul>
+        {#each guides as guide}
+            <li>
+                <a href="/">{guide.title}</a>
+            </li>
+        {/each}
     </ul>
 </div>
 
